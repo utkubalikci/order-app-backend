e895a0d6b56bf36f9c67669f02fa159ef6c5b2b9
utku <mehmetutkubalikci@gmail.com>
2024-09-22 15:34:11 +0300

    update user

--- src/main/java/com/pitech/order_app_backend/entities/User.java
+++ src/main/java/com/pitech/order_app_backend/entities/User.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
-@Data
 public class User {
     @Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -22,4 +21,49 @@ public class User {
     String password;
 
     UserRole role;
+
+	public User(String fullName, String userName, String password, UserRole role) {
+		this.fullName = fullName;
+		this.userName = userName;
+		this.password = password;
+		this.role = role;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+    
+    
 }
