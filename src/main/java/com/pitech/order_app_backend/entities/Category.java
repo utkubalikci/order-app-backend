936861f6157981aa98a84404d9dfbcbc7314e19f
utku <mehmetutkubalikci@gmail.com>
2024-09-22 12:59:59 +0300

    entity update

--- src/main/java/com/pitech/order_app_backend/entities/Category.java
+++ src/main/java/com/pitech/order_app_backend/entities/Category.java
@@ -17,4 +17,34 @@ public class Category {
 	
 	@Lob
 	String description;
+	
+	String imageUrl;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
 }
--- src/main/java/com/pitech/order_app_backend/entities/Product.java
+++ src/main/java/com/pitech/order_app_backend/entities/Product.java
@@ -19,21 +19,77 @@ import jakarta.persistence.ManyToOne;
 @Entity
 public class Product {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	String name;
-	
+
 	int stock;
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name="category_id", nullable=false)
+	@JoinColumn(name = "category_id", nullable = false)
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	@JsonIgnore
 	Category category;
-	
+
 	private BigDecimal price;
-	
+
 	@Lob
 	String description;
+	
+	String imageUrl;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+	
 }
