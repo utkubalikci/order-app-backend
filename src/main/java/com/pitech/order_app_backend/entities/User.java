7c1b0561b38410889ff1d759223e21ca66b50621
utku <mehmetutkubalikci@gmail.com>
2024-09-22 15:35:08 +0300

    update user

--- src/main/java/com/pitech/order_app_backend/entities/User.java
+++ src/main/java/com/pitech/order_app_backend/entities/User.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 @Entity
 public class User {
